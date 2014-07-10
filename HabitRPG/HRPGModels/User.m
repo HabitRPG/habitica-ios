75dd21628093a429eb4686ea9a61bf17523a5f78
Phillip Thelen <viirus@pherth.net>
2014-07-10 02:10:20 +0200

    fix color for user object

--- HabitRPG/HRPGModels/User.m
+++ HabitRPG/HRPGModels/User.m
@@ -378,6 +378,10 @@
 }
 
 -(UIColor *)contributorColor {
+    if (self.backerNpc) {
+        return [UIColor colorWithRed:1.0f green:0.22f blue:0.22f alpha:1.0f];
+    }
+    
     if ([self.contributorLevel integerValue] == 0) {
         
     } else if ([self.contributorLevel integerValue] <= 2) {
@@ -387,9 +391,9 @@
     } else if ([self.contributorLevel integerValue] <= 6) {
         return [UIColor colorWithRed:0.067 green:0.357 blue:0.635 alpha:1.000];
     } else if ([self.contributorLevel integerValue] <= 7) {
-        [UIColor colorWithRed:0.451 green:0.071 blue:0.706 alpha:1.000];
+        return [UIColor colorWithRed:0.451 green:0.071 blue:0.706 alpha:1.000];
     } else if ([self.contributorLevel integerValue] <= 8) {
-        [UIColor colorWithRed:1.000 green:0.506 blue:0.000 alpha:1.000];
+        return [UIColor colorWithRed:1.000 green:0.506 blue:0.000 alpha:1.000];
     }
     return [UIColor grayColor];
 }
