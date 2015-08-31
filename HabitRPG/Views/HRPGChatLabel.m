d42dbd786f6187669338c2ad8a8d02761b90acc2
Joshua Moody <joshuajmoody@gmail.com>
2015-08-31 16:46:55 +0200

    ChatLabel: can detect touch on URL

--- HabitRPG/Views/HRPGChatLabel.m
+++ HabitRPG/Views/HRPGChatLabel.m
@@ -6,6 +6,10 @@
 
 - (void) findURLRanges:(NSString *) text;
 - (void) handleTapWithRecognizer:(UITapGestureRecognizer *) recognizer;
+- (BOOL) tapAtPoint:(CGPoint) point wasWithinRange:(NSRange) range;
+
+- (CGPoint) offsetForTextContainerWithBoundingBox:(CGRect) boundingBox;
+- (CGPoint) pointByApplyingOffset:(CGPoint) offset toPoint:(CGPoint) point;
 
 @end
 
@@ -29,6 +33,54 @@
 
 - (void) handleTapWithRecognizer:(UITapGestureRecognizer *) recognizer {
   NSLog(@"Handling tap on chat label");
+}
+
+- (CGPoint) offsetForTextContainerWithBoundingBox:(CGRect) boundingBox {
+  CGSize size = self.bounds.size;
+  CGSize boxSize = boundingBox.size;
+  CGPoint boxOrigin = boundingBox.origin;
+
+  return CGPointMake((size.width - boxSize.width)/2 - boxOrigin.x,
+                     (size.height - boxSize.height)/2 - boxOrigin.y);
+}
+
+- (CGPoint) pointByApplyingOffset:(CGPoint) offset toPoint:(CGPoint) point {
+  return CGPointMake(point.x - offset.x, point.y - offset.y);
+}
+
+- (BOOL) tapAtPoint:(CGPoint) point wasWithinRange:(NSRange) range {
+
+  CGSize size = self.bounds.size;
+  NSAttributedString *attributedText = self.attributedText;
+
+  NSLayoutManager *layoutManager = [[NSLayoutManager alloc] init];
+  NSTextContainer *textContainer = [[NSTextContainer alloc] initWithSize:size];
+  NSTextStorage *textStorage = [[NSTextStorage alloc]
+                                initWithAttributedString:attributedText];
+
+  // Will have consequences if string has multiple fonts...
+  [textStorage addAttribute:NSFontAttributeName
+                      value:self.font
+                      range:NSMakeRange(0, attributedText.length)];
+
+
+  [layoutManager addTextContainer:textContainer];
+  [textStorage addLayoutManager:layoutManager];
+
+  textContainer.lineFragmentPadding = 0.0;
+  textContainer.lineBreakMode = self.lineBreakMode;
+  textContainer.maximumNumberOfLines = self.numberOfLines;
+
+
+  CGRect textBoundingBox = [layoutManager usedRectForTextContainer:textContainer];
+  CGPoint offset = [self offsetForTextContainerWithBoundingBox:textBoundingBox];
+
+  CGPoint locationOfTouch = [self pointByApplyingOffset:offset toPoint:point];
+
+  NSUInteger index = [layoutManager characterIndexForPoint:locationOfTouch
+                                           inTextContainer:textContainer
+                  fractionOfDistanceBetweenInsertionPoints:nil];
+  return NSLocationInRange(index, range);
 }
 
 - (void) setAttributedText:(NSAttributedString *)attributedText {
@@ -55,4 +107,3 @@
 }
 
 @end
-
--- HabiticaTests/Views/HRPGChatLabelTest.m
+++ HabiticaTests/Views/HRPGChatLabelTest.m
@@ -2,16 +2,103 @@
 #import <XCTest/XCTest.h>
 #import "HRPGChatLabel.h"
 
+@interface NSString (XCTEST)
+
+- (CGSize) sizeOfStringWithFont:(UIFont *) aFont
+              constrainedToSize:(CGSize) aSize;
+
+- (CGSize) sizeOfStringWithFont:(UIFont *) aFont
+              constrainedToSize:(CGSize) aSize
+                  lineBreakMode:(NSLineBreakMode) aLineBreakMode;
+
+- (CGSize) sizeOfStringWithFont:(UIFont *) aFont
+                    minFontSize:(CGFloat) aMinSize
+                 actualFontSize:(CGFloat *) aActualFontSize
+                       forWidth:(CGFloat) aWidth
+                  lineBreakMode:(NSLineBreakMode) aLineBreakMode;
+
+
+@end
+
+@implementation NSString (XCTEST)
+
+- (CGSize) sizeOfStringWithFont:(UIFont *) aFont
+              constrainedToSize:(CGSize) aSize {
+  return [self sizeOfStringWithFont:aFont
+                  constrainedToSize:aSize
+                      lineBreakMode:NSLineBreakByWordWrapping];
+}
+
+- (CGSize) sizeOfStringWithFont:(UIFont *) aFont
+              constrainedToSize:(CGSize) aSize
+                  lineBreakMode:(NSLineBreakMode) aLineBreakMode {
+  NSTextStorage *textStorage = [[NSTextStorage alloc] initWithString:self];
+  NSTextContainer *textContainer = [[NSTextContainer alloc] initWithSize:aSize];
+  NSLayoutManager *layoutManager = [[NSLayoutManager alloc] init];
+  [layoutManager addTextContainer:textContainer];
+  [textStorage addLayoutManager:layoutManager];
+  [textStorage addAttribute:NSFontAttributeName
+                      value:aFont
+                      range:NSMakeRange(0, self.length)];
+  [textContainer setLineBreakMode:aLineBreakMode];
+  [textContainer setLineFragmentPadding:0.0];
+  (void)[layoutManager glyphRangeForTextContainer:textContainer];
+  return [layoutManager usedRectForTextContainer:textContainer].size;
+}
+
+- (CGSize) sizeOfStringWithFont:(UIFont *) aFont
+                    minFontSize:(CGFloat) aMinSize
+                 actualFontSize:(CGFloat *) aActualFontSize
+                       forWidth:(CGFloat) aWidth
+                  lineBreakMode:(NSLineBreakMode) aLineBreakMode {
+
+  CGFloat currentFontSize = aFont.pointSize;
+  CGSize targetSize = CGSizeMake(CGFLOAT_MAX, CGFLOAT_MAX);
+  CGSize currentSize = CGSizeZero;
+
+  CGFloat lineHeight = CGFLOAT_MAX;
+
+  do {
+    UIFont *currentFont = [aFont fontWithSize:currentFontSize];
+    NSTextStorage *textStorage = [[NSTextStorage alloc] initWithString:self];
+    NSTextContainer *textContainer = [[NSTextContainer alloc] initWithSize:targetSize];
+    NSLayoutManager *layoutManager = [[NSLayoutManager alloc] init];
+    [layoutManager addTextContainer:textContainer];
+    [textStorage addLayoutManager:layoutManager];
+    [textStorage addAttribute:NSFontAttributeName
+                        value:currentFont
+                        range:NSMakeRange(0, self.length)];
+    [textContainer setLineBreakMode:aLineBreakMode];
+    [textContainer setLineFragmentPadding:0.0];
+    (void)[layoutManager glyphRangeForTextContainer:textContainer];
+
+    currentSize = [layoutManager usedRectForTextContainer:textContainer].size;
+    if (lineHeight == CGFLOAT_MAX) {  lineHeight = currentSize.height; }
+
+    if (currentFontSize - 1.0f < aMinSize) {  break; }
+    currentFontSize -= 1.0f;
+  } while (currentSize.width > aWidth);
+  *aActualFontSize = currentFontSize;
+  return CGSizeMake(currentSize.width, lineHeight);
+}
+
+@end
+
 @interface HRPGChatLabel (XCTESTS)
 
 - (NSArray *) URLRanges;
 - (void) findURLRanges:(NSString *) text;
+
+- (CGPoint) offsetForTextContainerWithBoundingBox:(CGRect) boundingBox;
+- (CGPoint) pointByApplyingOffset:(CGPoint) offset toPoint:(CGPoint) point;
+- (BOOL) tapAtPoint:(CGPoint) point wasWithinRange:(NSRange) range;
 
 @end
 
 @interface HRPGChatLabelTest : XCTestCase
 
 @property(nonatomic, strong) HRPGChatLabel *label;
+@property(nonatomic, strong) UIFont *font;
 
 @end
 
@@ -20,11 +107,49 @@
 - (void) setUp {
   [super setUp];
   self.label = [[HRPGChatLabel alloc] initWithFrame:CGRectZero];
+  self.font = [UIFont fontWithName:@"Avenir" size:18];
+  self.label.font = self.font;
 }
 
 - (void) tearDown {
   [super tearDown];
 }
+
+#pragma mark - Helpers
+
+- (CGRect) frameAfterConfiguringLabelWithText:(NSString *) text {
+  NSAttributedString *attributed;
+  attributed = [[NSAttributedString alloc]
+                initWithString:text];
+
+  self.label.attributedText = attributed;
+
+  CGSize size = [text sizeOfStringWithFont:self.font
+                         constrainedToSize:CGSizeMake(CGFLOAT_MAX, CGFLOAT_MAX)];
+
+  CGRect frame = CGRectMake(0, 0, size.width + 20, size.height + 16);
+  self.label.frame = frame;
+  return frame;
+}
+
+- (NSArray *) arrayOfURLRanges:(NSString *) text {
+  NSError *error = NULL;
+  NSDataDetector *detector;
+  detector = [NSDataDetector dataDetectorWithTypes:NSTextCheckingTypeLink
+                                             error:&error];
+
+  if (!detector) {
+    NSLog(@"Error detecting URLs in chat text: %@",
+          [error localizedDescription]);
+    return nil;
+  }
+
+  return [detector matchesInString:text
+                           options:0
+                             range:NSMakeRange(0, [text length])];
+}
+
+#pragma mark - Tests
 
 - (void) testLabelHasRecognizer {
   NSArray *recognizers = [self.label gestureRecognizers];
@@ -35,6 +160,83 @@
 
 - (void) testAllowsUserInteraction {
   XCTAssertEqualObjects(@([[self label] isUserInteractionEnabled]), @(1));
+}
+
+- (void) testOffestForTextContainer {
+  CGRect frame = CGRectMake(0, 0, 300, 80);
+  self.label.frame = frame;
+
+  CGRect box = CGRectMake(10, 10, 260, 20);
+
+  CGPoint actual = [self.label offsetForTextContainerWithBoundingBox:box];
+  CGPoint expected = CGPointMake(10, 20);
+
+  XCTAssertEqualObjects(@(actual.x), @(expected.x));
+  XCTAssertEqualObjects(@(actual.y), @(expected.y));
+}
+
+- (void) testPointByApplyingOffset {
+  CGPoint offset = CGPointMake(10, 20);
+  CGPoint point = CGPointMake(20, 40);
+
+  CGPoint actual = [self.label pointByApplyingOffset:offset toPoint:point];
+  CGPoint expected = CGPointMake(10, 20);
+
+  XCTAssertEqualObjects(@(actual.x), @(expected.x));
+  XCTAssertEqualObjects(@(actual.y), @(expected.y));
+}
+
+- (void) testTapAtPointWasWithRangeReturnsNO {
+  NSString *url = @"https://gist.github.com/jmoody/ccd1d44085f829cc44e9";
+  NSString *text = [NSString stringWithFormat:@"This gist! %@ shows an example",
+                    url];
+
+  CGRect frame = [self frameAfterConfiguringLabelWithText:text];
+  NSTextCheckingResult *match = [self arrayOfURLRanges:text][0];
+  NSRange range = match.range;
+
+  CGPoint touch;
+
+  // Touches the '!' in 'gist!:'
+  touch = CGPointMake(80, frame.size.height/2);
+  XCTAssertFalse([self.label tapAtPoint:touch wasWithinRange:range]);
+
+  // Touches the 'x' in 'example'
+  touch = CGPointMake(635, frame.size.height/2);
+  XCTAssertFalse([self.label tapAtPoint:touch wasWithinRange:range]);
+
+  // Touches above the link.
+  touch = CGPointMake(400, 4);
+  XCTAssertFalse([self.label tapAtPoint:touch wasWithinRange:range]);
+
+  // Touches below the link.
+  touch = CGPointMake(400, frame.size.height - 4);
+  XCTAssertFalse([self.label tapAtPoint:touch wasWithinRange:range]);
+}
+
+- (void) testTapAtPointWasWithRangeReturnsYES {
+  NSString *url = @"https://gist.github.com/jmoody/ccd1d44085f829cc44e9";
+  NSString *text = [NSString stringWithFormat:@"This gist! %@ shows an example",
+                    url];
+
+  CGRect frame = [self frameAfterConfiguringLabelWithText:text];
+  NSTextCheckingResult *match = [self arrayOfURLRanges:text][0];
+  NSRange range = match.range;
+
+  CGPoint touch;
+
+  // Touches the ':' in 'http:'.
+  touch = CGPointMake(130, frame.size.height/2);
+  XCTAssertTrue([self.label tapAtPoint:touch wasWithinRange:range]);
+
+
+  // Touches the 'j' in 'jmoody'.
+  touch = CGPointMake(280, frame.size.height/2);
+  XCTAssertTrue([self.label tapAtPoint:touch wasWithinRange:range]);
+
+  // Touches the '9' in at the end of the URL.
+  touch = CGPointMake(534, frame.size.height/2);
+  XCTAssertTrue([self.label tapAtPoint:touch wasWithinRange:range]);
 }
 
 - (void) testFindURLRangeNone {
@@ -99,4 +301,3 @@
 }
 
 @end
-
