94019e71bbbe4ff8c25db45dcbe33e4868f08a57
Phillip Thelen <viirus@pherth.net>
2015-09-23 21:47:11 +0200

    add copyable label

---
+++ HabitRPG/Views/HRPGCopyLabel.h
@@ -0,0 +1,13 @@
+//
+//  HRPGCopyLabel.h
+//  Habitica
+//
+//  Created by Phillip Thelen on 21/09/15.
+//  Copyright © 2015 Phillip Thelen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HRPGCopyLabel : UILabel
+
+@end
