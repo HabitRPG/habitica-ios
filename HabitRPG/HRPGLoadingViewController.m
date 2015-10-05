4b8bc6049b5a17459b9a8cc53fce9821b04454c3
Phillip Thelen <viirus@pherth.net>
2015-10-05 21:22:55 +0200

    Don't resume setup

--- HabitRPG/HRPGLoadingViewController.m
+++ HabitRPG/HRPGLoadingViewController.m
@@ -84,14 +84,7 @@
     if ([keyChain stringForKey:@"id"] == nil || [[keyChain stringForKey:@"id"] isEqualToString:@""]) {
         [self performSegueWithIdentifier:@"LoginSegue" sender:self];
     } else {
-        HRPGAppDelegate *appdelegate = (HRPGAppDelegate *) [[UIApplication sharedApplication] delegate];
-        HRPGManager *manager = appdelegate.sharedManager;
-        User *user = [manager getUser];
-        if ([user.lastSetupStep integerValue] != HRPGAvatarSetupStepsTasks) {
-            [self performSegueWithIdentifier:@"SetupSegue" sender:self];
-        } else {
-            [self performSegueWithIdentifier:@"InitialSegue" sender:self];
-        }
+        [self performSegueWithIdentifier:@"InitialSegue" sender:self];
     }
 }
 
@@ -100,15 +93,6 @@
         UINavigationController *navigationViewController = (UINavigationController*)segue.destinationViewController;
         HRPGLoginViewController *loginViewController = (HRPGLoginViewController*)navigationViewController.topViewController;
         loginViewController.isRootViewController = YES;
-    } else if ([segue.identifier isEqualToString:@"SetupSegue"]) {
-        UINavigationController *navController = segue.destinationViewController;
-        HRPGAvatarSetupViewController *avatarSetupViewController = (HRPGAvatarSetupViewController*)navController.topViewController;
-        HRPGAppDelegate *appdelegate = (HRPGAppDelegate *) [[UIApplication sharedApplication] delegate];
-        HRPGManager *manager = appdelegate.sharedManager;
-        User *user = [manager getUser];
-        avatarSetupViewController.lastCompletedStep = [user.lastSetupStep integerValue];
-        avatarSetupViewController.user = user;
-        avatarSetupViewController.managedObjectContext = manager.getManagedObjectContext;
     }
 }
 
