4bc44f75399dbe417bee69dbb4a9b32167230078
Phillip Thelen <viirus@pherth.net>
2015-10-21 19:50:52 +0200

    update facebook sdk

---
+++ HabitRPG/Views/HRPGHintView.h
@@ -0,0 +1,13 @@
+//
+//  HRPGHintView.h
+//  Habitica
+//
+//  Created by Phillip Thelen on 21/10/15.
+//  Copyright © 2015 Phillip Thelen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HRPGHintView : UIView
+
+@end
---
+++ HabitRPG/Views/HRPGHintView.m
@@ -0,0 +1,21 @@
+//
+//  HRPGHintView.m
+//  Habitica
+//
+//  Created by Phillip Thelen on 21/10/15.
+//  Copyright © 2015 Phillip Thelen. All rights reserved.
+//
+
+#import "HRPGHintView.h"
+
+@implementation HRPGHintView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
