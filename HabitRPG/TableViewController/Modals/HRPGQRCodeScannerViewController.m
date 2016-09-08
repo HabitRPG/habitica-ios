3ab379c31edc36805305c297d299ef699c26e6eb
Phillip Thelen <viirus@pherth.net>
2016-09-08 17:10:54 +0200

    fix crash in qr code scanner

--- HabitRPG/TableViewController/Modals/HRPGQRCodeScannerViewController.m
+++ HabitRPG/TableViewController/Modals/HRPGQRCodeScannerViewController.m
@@ -79,25 +79,26 @@
     self.device = [AVCaptureDevice defaultDeviceWithMediaType:AVMediaTypeVideo];
     
     self.input = [AVCaptureDeviceInput deviceInputWithDevice:self.device error:nil];
-    
-    self.session = [[AVCaptureSession alloc] init];
-    
-    self.output = [[AVCaptureMetadataOutput alloc] init];
-    [self.session addOutput:self.output];
-    [self.session addInput:self.input];
-    
-    [self.output setMetadataObjectsDelegate:self queue:dispatch_get_main_queue()];
-    self.output.metadataObjectTypes = @[AVMetadataObjectTypeQRCode];
-    
-    self.preview = [AVCaptureVideoPreviewLayer layerWithSession:self.session];
-    self.preview.videoGravity = AVLayerVideoGravityResizeAspectFill;
-    self.preview.frame = CGRectMake(0, 0, self.view.frame.size.width, self.view.frame.size.height);
-    
-    AVCaptureConnection *con = self.preview.connection;
-    
-    con.videoOrientation = [self getAVOrientation];
-    
-    [self.view.layer insertSublayer:self.preview atIndex:0];
+    if (self.input != nil) {
+        self.session = [[AVCaptureSession alloc] init];
+        
+        self.output = [[AVCaptureMetadataOutput alloc] init];
+        [self.session addOutput:self.output];
+        [self.session addInput:self.input];
+        
+        [self.output setMetadataObjectsDelegate:self queue:dispatch_get_main_queue()];
+        self.output.metadataObjectTypes = @[AVMetadataObjectTypeQRCode];
+        
+        self.preview = [AVCaptureVideoPreviewLayer layerWithSession:self.session];
+        self.preview.videoGravity = AVLayerVideoGravityResizeAspectFill;
+        self.preview.frame = CGRectMake(0, 0, self.view.frame.size.width, self.view.frame.size.height);
+        
+        AVCaptureConnection *con = self.preview.connection;
+        
+        con.videoOrientation = [self getAVOrientation];
+        
+        [self.view.layer insertSublayer:self.preview atIndex:0];
+    }
 }
 
 #pragma mark -
