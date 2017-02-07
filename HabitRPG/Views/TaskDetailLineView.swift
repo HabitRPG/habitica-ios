8a231fc91d9d3afe58383ea9a9dbbca794cb8a0d
Phillip Thelen <viirus@pherth.net>
2017-02-07 11:47:00 +0100

    Update icons

---
+++ HabitRPG/Views/TaskDetailLineView.swift
@@ -0,0 +1,21 @@
+//
+//  TaskDetailLineView.swift
+//  Habitica
+//
+//  Created by Phillip Thelen on 06/02/2017.
+//  Copyright © 2017 Phillip Thelen. All rights reserved.
+//
+
+import UIKit
+
+class TaskDetailLineView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
