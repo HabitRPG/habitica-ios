261727248474ec16d8c8699be97ebd74c7ad8179
Phillip Thelen <viirus@pherth.net>
2017-07-27 19:29:06 +0200

    add new Intro

--- HabitRPG/UI/Intro/IntroViewController.swift
+++ HabitRPG/UI/Intro/IntroViewController.swift
@@ -9,106 +9,62 @@
 import UIKit
 import EAIntroView
 
-class IntroViewController: UIViewController, EAIntroDelegate {
+class IntroViewController: UIViewController, UIScrollViewDelegate {
 
-    var intro: EAIntroView?
+    @IBOutlet weak var endButton: UIButton!
+    @IBOutlet weak var skipButton: UIButton!
+    @IBOutlet weak var indicatorStackView: UIStackView!
+    @IBOutlet weak var scrollView: UIScrollView!
 
-    //swiftlint:disable:next function_body_length
     override func viewDidLoad() {
-        let titleposition = (self.view.frame.size.height / 2) - (self.view.frame.size.height / 16)
-
-        let page1 = EAIntroPage()
-        page1.title = "Welcome to Habitica".localized
-        page1.titlePositionY = titleposition
-        page1.titleFont = UIFont.boldSystemFont(ofSize: 20.0)
-        let userCount = "2,000,000"
-        page1.desc = "Join over \(userCount) people having fun while getting things done. Create an avatar and track your real-life tasks.".localized
-        page1.descPositionY = titleposition - 24
-        page1.descFont = UIFont.systemFont(ofSize: 14.0)
-        page1.titleIconView = UIImageView(image: #imageLiteral(resourceName: "IntroPage1"))
-        page1.titleIconPositionY = titleposition - 90
-
-        weak var weakPage1 = page1
-        page1.onPageDidLoad = {
-            weakPage1?.titleIconView.alpha = 0
-        }
-        page1.onPageDidDisappear = {
-            weakPage1?.titleIconView.alpha = 0
-        }
-        page1.onPageDidAppear = {
-            UIView .animate(withDuration: 0.8, animations: {
-                weakPage1?.titleIconView.alpha = 1
-            })
-        }
-
-        let page2 = EAIntroPage()
-        page2.title = "Game Progress = Life Progress".localized
-        page2.titlePositionY = titleposition
-        page2.titleFont = UIFont.boldSystemFont(ofSize: 20.0)
-        page2.desc = "Unlock features in the game by checking off your real-life tasks. Earn armor, pets, and more to reward you for meeting your goals!".localized
-        page2.descPositionY = titleposition - 24
-        page2.descFont = UIFont.systemFont(ofSize: 14.0)
-        page2.titleIconView = UIImageView(image: #imageLiteral(resourceName: "IntroPage2"))
-        page2.titleIconPositionY = titleposition - 220
-        weak var weakPage2 = page2
-        page2.onPageDidLoad = {
-            weakPage2?.titleIconView.alpha = 0
-        }
-        page2.onPageDidDisappear = {
-            weakPage2?.titleIconView.alpha = 0
-        }
-        page2.onPageDidAppear = {
-            UIView .animate(withDuration: 0.8, animations: {
-                weakPage2?.titleIconView.alpha = 1
-            })
-        }
-        let page3 = EAIntroPage()
-        page3.titlePositionY = titleposition
-        page3.titleFont = UIFont.boldSystemFont(ofSize: 20.0)
-        page3.title = "Get Social and Fight Monsters".localized
-        page3.desc = "Keep your goals on track with help from your friends. Support each other in life and in battle as you improve together!".localized
-        page3.descPositionY = titleposition - 24
-        page3.descFont = UIFont.systemFont(ofSize: 14.0)
-        page3.titleIconView = UIImageView(image: #imageLiteral(resourceName: "IntroPage3"))
-        page3.titleIconPositionY = titleposition - 230
-
-        weak var weakPage3 = page3
-        page3.onPageDidLoad = {
-            weakPage3?.titleIconView.alpha = 0
-        }
-        page3.onPageDidDisappear = {
-            weakPage3?.titleIconView.alpha = 0
-        }
-        page3.onPageDidAppear = {
-            UIView .animate(withDuration: 0.8, animations: {
-                weakPage3?.titleIconView.alpha = 1
-            })
-        }
-
-        self.intro = EAIntroView(frame: self.view.frame, andPages: [page1, page2, page3])
-        self.intro?.bgImage = #imageLiteral(resourceName: "IntroBackground")
-        self.intro?.delegate = self
-
         super.viewDidLoad()
+
+        endButton.isHidden = true
     }
 
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
-        self.intro?.show(in: self.view)
     }
 
     func introDidFinish(_ introView: EAIntroView!, wasSkipped: Bool) {
         self.performSegue(withIdentifier: "LoginSegue", sender: self)
     }
 
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        if segue.identifier == "LoginSegue" {
-            if let navigationController = segue.destination as? UINavigationController {
-                if let loginViewController = navigationController.topViewController as? LoginTableViewController {
-                    loginViewController.isRootViewController = true
+    func scrollViewDidEndDecelerating(_ scrollView: UIScrollView) {
+        let currentPage = getCurrentPage()
+        updateIndicator(currentPage)
 
+        if currentPage == 2 {
+            skipButton.isHidden = true
+            endButton.isHidden = false
+        } else {
+            skipButton.isHidden = false
+            endButton.isHidden = true
+            
+        }
+    }
+
+    func updateIndicator(_ currentPage: Int) {
+        for (index, element) in indicatorStackView.arrangedSubviews.enumerated() {
+            if let indicatorView = element as? UIImageView {
+                if index == currentPage {
+                    indicatorView.image = #imageLiteral(resourceName: "indicatorDiamondSelected")
+                } else {
+                    indicatorView.image = #imageLiteral(resourceName: "indicatorDiamondUnselected")
                 }
             }
+        }
+    }
+
+    func getCurrentPage() -> Int {
+        return Int(scrollView.contentOffset.x / scrollView.frame.size.width)
+    }
+
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "LoginSegue" {
+                if let loginViewController = segue.destination as? LoginTableViewController {
+                    loginViewController.isRootViewController = true
+                }
         }
     }
 }
