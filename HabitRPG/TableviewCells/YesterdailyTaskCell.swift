f3669d39388853c31979d775ea5ce20accc59a50
Phillip Thelen <viirus@pherth.net>
2018-06-19 13:02:06 +0200

    fix layouting issues with yesterdailies dialog

--- HabitRPG/TableviewCells/YesterdailyTaskCell.swift
+++ HabitRPG/TableviewCells/YesterdailyTaskCell.swift
@@ -8,16 +8,16 @@
 
 import UIKit
 import Habitica_Models
+import PinLayout
 
 class YesterdailyTaskCell: UITableViewCell {
 
     @IBOutlet weak var wrapperView: UIView!
     @IBOutlet weak var checkbox: CheckboxView!
     @IBOutlet weak var titleTextView: UILabel!
-    @IBOutlet weak var checklistStackview: UIStackView!
     
     var onChecklistItemChecked: ((ChecklistItemProtocol) -> Void)?
-    var checklistItems: [ChecklistItemProtocol]?
+    var checklistItems: [(UIView, ChecklistItemProtocol)] = []
 
     override func awakeFromNib() {
         super.awakeFromNib()
@@ -27,14 +27,15 @@ class YesterdailyTaskCell: UITableViewCell {
     }
 
     func configure(task: TaskProtocol) {
+        backgroundColor = UIColor.gray700()
         checkbox.configure(task: task)
         titleTextView.text = task.text?.unicodeEmoji
 
-        checklistStackview.subviews.forEach { view in
+        checklistItems.forEach({ (view, _) in
             view.removeFromSuperview()
-        }
+        })
+        checklistItems = []
 
-        checklistItems = task.checklist
         for checklistItem in task.checklist {
             if let view = UIView.fromNib(nibName: "YesterdailyChecklistItem") {
                 let label = view.viewWithTag(2) as? UILabel
@@ -47,7 +48,8 @@ class YesterdailyTaskCell: UITableViewCell {
                         checked(checklistItem)
                     }
                 }
-                checklistStackview.addArrangedSubview(view)
+                wrapperView.addSubview(view)
+                checklistItems.append((view, checklistItem))
                 let recognizer = UITapGestureRecognizer(target: self, action: #selector(YesterdailyTaskCell.handleChecklistTap(recognizer:)))
                 view.addGestureRecognizer(recognizer)
             }
@@ -56,16 +58,54 @@ class YesterdailyTaskCell: UITableViewCell {
 
     override func layoutSubviews() {
         super.layoutSubviews()
+        layout()
+    }
+    
+    override func sizeThatFits(_ size: CGSize) -> CGSize {
+        contentView.pin.width(size.width)
+        layout()
+        return CGSize(width: contentView.frame.width, height: wrapperView.frame.height + 8)
+    }
+    
+    private func layout() {
+        wrapperView.pin.horizontally()
+        checkbox.pin.width(55).start().top()
+        titleTextView.pin.after(of: checkbox).marginStart(10).end(4).top().sizeToFit(.width)
+        let textHeight = max(titleTextView.frame.size.height + 8, 48)
+        checkbox.pin.height(textHeight)
+        titleTextView.pin.height(textHeight)
+        var checklistHeight = CGFloat(0)
+        var topEdge = titleTextView.edge.bottom
+        for (view, _) in checklistItems {
+            guard let label = view.viewWithTag(2) as? UILabel else {
+                continue
+            }
+            guard let itemCheckbox = view.viewWithTag(1) as? CheckboxView else {
+                continue
+            }
+            view.pin.top(to: topEdge).horizontally()
+            itemCheckbox.pin.width(55).start().top()
+            label.pin.after(of: itemCheckbox).marginStart(10).end(4).top().sizeToFit(.width)
+            let itemHeight = max(label.frame.size.height + 8, 40)
+            label.pin.height(itemHeight)
+            itemCheckbox.pin.height(itemHeight)
+            view.pin.height(itemHeight)
+            topEdge = view.edge.bottom
+            checklistHeight += view.frame.size.height
+        }
+        let height = textHeight + checklistHeight + 1
+        wrapperView.pin.height(height).top(4)
     }
 
     @objc
     func handleChecklistTap(recognizer: UITapGestureRecognizer) {
-        for (index, view) in checklistStackview.arrangedSubviews.enumerated() where view == recognizer.view {
-            if let checked = self.onChecklistItemChecked, let item = checklistItems?[index] {
-                checked(item)
+        if let (_, checklistItem) = checklistItems.first(where: { (view, _) -> Bool in
+            return view == recognizer.view
+        }) {
+            if let checked = self.onChecklistItemChecked {
+                checked(checklistItem)
             }
             return
         }
     }
-    
 }
