5f0952580f30a25dac3316d80912601ecbf53d93
Phillip Thelen <viirus@pherth.net>
2018-07-17 14:23:00 +0200

    Improve markdown support. Fixes #734

---
+++ HabitRPG/Extensions/UIFont-Extensions.swift
@@ -0,0 +1,24 @@
+//
+//  UIFont-Extensions.swift
+//  Habitica
+//
+//  Created by Phillip Thelen on 17.07.18.
+//  Copyright © 2018 HabitRPG Inc. All rights reserved.
+//
+
+import Foundation
+extension UIFont {
+    
+    func withTraits(traits: UIFontDescriptor.SymbolicTraits...) -> UIFont {
+        if let descriptor = self.fontDescriptor.withSymbolicTraits(UIFontDescriptorSymbolicTraits(traits)) {
+            return UIFont(descriptor: descriptor, size: 0)
+        } else {
+            return self
+        }
+    }
+    
+    static func boldItalicSystemFont(ofSize: CGFloat) -> UIFont {
+        return UIFont.systemFont(ofSize: ofSize).withTraits(traits: .traitBold, .traitItalic)
+    }
+    
+}
--- HabitRPG/TableviewCells/Social/ChatTableViewCell.swift
+++ HabitRPG/TableviewCells/Social/ChatTableViewCell.swift
@@ -10,6 +10,7 @@ import UIKit
 import DateTools
 import PinLayout
 import Habitica_Models
+import Down
 
 class ChatTableViewCell: UITableViewCell {
     
@@ -141,10 +142,8 @@ class ChatTableViewCell: UITableViewCell {
         
         setTimeStamp(date: chatMessage.timestamp)
         
-        if chatMessage.attributedText?.length ?? 0 > 0 {
-            messageTextView.attributedText = chatMessage.attributedText
-        } else {
-            messageTextView.text = chatMessage.text?.unicodeEmoji
+        if let text = chatMessage.text {
+            messageTextView.attributedText = try? Down(markdownString: text).toHabiticaAttributedString()
         }
         
         if previousMessage?.isValid == true, previousMessage?.userID == chatMessage.userID {
@@ -197,10 +196,8 @@ class ChatTableViewCell: UITableViewCell {
         
         setTimeStamp(date: inboxMessage.timestamp)
         
-        if inboxMessage.attributedText?.length ?? 0 > 0 {
-            messageTextView.attributedText = inboxMessage.attributedText
-        } else {
-            messageTextView.text = inboxMessage.text?.unicodeEmoji
+        if let text = inboxMessage.text {
+            messageTextView.attributedText = try? Down(markdownString: text).toHabiticaAttributedString()
         }
         
         if previousMessage?.sent == inboxMessage.sent {
