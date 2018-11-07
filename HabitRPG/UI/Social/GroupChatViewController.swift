fbdde36e4e2f04231ef01c1793b1ad0a15c2bf40
Phillip Thelen <viirus@pherth.net>
2018-11-07 13:48:50 +0100

    change username mentioning handling

--- HabitRPG/UI/Social/GroupChatViewController.swift
+++ HabitRPG/UI/Social/GroupChatViewController.swift
@@ -180,9 +180,7 @@ class GroupChatViewController: SLKTextViewController {
     }
     
     func configureReplyTo(_ username: String?) {
-        if !textView.text.contains("@\(username ?? "")") {
-            textView.text = "@\(username ?? "") \(textView.text ?? "")"
-        }
+        textView.text = "\(textView.text ?? "") @\(username ?? "")"
         textView.becomeFirstResponder()
         textView.selectedRange = NSRange(location: self.textView.text.count, length: 0)
     }
