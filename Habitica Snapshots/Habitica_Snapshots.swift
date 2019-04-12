7c3249b188dc8177018d2036d4eb714dd2e2171e
Phillip Thelen <viirus@pherth.net>
2019-04-12 16:21:46 +0200

    start adding code for taking automated screenshots

--- Habitica Snapshots/Habitica_Snapshots.swift
+++ Habitica Snapshots/Habitica_Snapshots.swift
@@ -7,13 +7,19 @@
 //
 
 import XCTest
+import KeychainAccess
 
 class Habitica_Snapshots: XCTestCase {
 
     override func setUp() {
         let app = XCUIApplication()
         setupSnapshot(app)
+        let keychain = Keychain(server: "https://habitica.com", protocolType: .https)
+            .accessibility(.afterFirstUnlock)
+        
+        let defaults = UserDefaults.standard
         app.launch()
+        
     }
 
     override func tearDown() {
@@ -21,7 +27,39 @@ class Habitica_Snapshots: XCTestCase {
     }
 
     func testExample() {
-        snapshot("01Intro")
+        snapshot("Habits")
+        
+        let app = XCUIApplication()
+        let tabBarsQuery = app.tabBars
+        tabBarsQuery.buttons["Dailies"].tap()
+        sleep(2)
+        snapshot("Dailies")
+        tabBarsQuery.buttons["To-Dos"].tap()
+        sleep(2)
+        snapshot("T-Dos")
+        tabBarsQuery.buttons["Menu"].tap()
+        
+        let tablesQuery = app.tables
+        tablesQuery/*@START_MENU_TOKEN@*/.staticTexts["Stable"]/*[[".cells.staticTexts[\"Stable\"]",".staticTexts[\"Stable\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
+        sleep(5)
+        snapshot("Stable")
+        app.navigationBars["Stable"].buttons["Back"].tap()
+        tablesQuery.staticTexts["Party"].tap()
+        app.navigationBars["Items"].buttons["Back"].tap()
+        
+        
+        app.scrollViews.otherElements.scrollViews.otherElements.staticTexts["Dilatory Distress, Part 2: Creatures of the Crevasse"].tap()
+        app.navigationBars["Quest"].buttons["Party"].tap()
+        sleep(2)
+        snapshot("Quest")
+        
+        let ourPartyElementsQuery = XCUIApplication().scrollViews.otherElements.scrollViews.otherElements.containing(.staticText, identifier:"Our Party")
+        let element = ourPartyElementsQuery.children(matching: .other).element(boundBy: 1)
+        element.children(matching: .other).element.swipeUp()
+        element.swipeUp()
+        ourPartyElementsQuery.children(matching: .other).element(boundBy: 3).children(matching: .other).element(boundBy: 1).swipeUp()
+        sleep(5)
+        snapshot("Members")
     }
 
 }
