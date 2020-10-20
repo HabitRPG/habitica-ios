60b4b1b4f537936110b6ac59b381d819bbc2718e
Phillip Thelen <phillip@habitica.com>
2020-10-20 14:47:22 +0200

    disable FB auto logging

---
+++ HabitRPG/UI/General/MaintenanceViewController.swift
@@ -0,0 +1,9 @@
+//
+//  MaintenanceViewController.swift
+//  Habitica
+//
+//  Created by Phillip Thelen on 20.10.20.
+//  Copyright © 2020 HabitRPG Inc. All rights reserved.
+//
+
+import Foundation
