4418ffe2d5ad4db7a4a5caea387ffe303e789250
Victor Rolando Sanchez Jara <roli.s91@gmail.com>
2021-11-23 10:58:26 -0300

    Modify HabiticaStyler for image markdown to use max width/height for images (#1109)

--- HabitRPG/Extensions/UIImage-Extensions.swift
+++ HabitRPG/Extensions/UIImage-Extensions.swift
@@ -36,4 +36,32 @@ extension UIImage {
         }
         return nil
     }
+    
+    // Resizable UIImage with max width/height from: https://stackoverflow.com/questions/24709244/how-do-set-a-width-and-height-of-an-image-in-swift
+    func resize(maxWidthHeight: Double) -> UIImage? {
+        let actualHeight = Double(size.height)
+        let actualWidth = Double(size.width)
+        var maxWidth = 0.0
+        var maxHeight = 0.0
+
+        if actualWidth > actualHeight {
+            maxWidth = maxWidthHeight
+            let per = (100.0 * maxWidthHeight / actualWidth)
+            maxHeight = (actualHeight * per) / 100.0
+        } else {
+            maxHeight = maxWidthHeight
+            let per = (100.0 * maxWidthHeight / actualHeight)
+            maxWidth = (actualWidth * per) / 100.0
+        }
+
+        let hasAlpha = true
+        let scale: CGFloat = 0.0
+
+        UIGraphicsBeginImageContextWithOptions(CGSize(width: maxWidth, height: maxHeight), !hasAlpha, scale)
+        self.draw(in: CGRect(origin: .zero, size: CGSize(width: maxWidth, height: maxHeight)))
+
+        let scaledImage = UIGraphicsGetImageFromCurrentImageContext()
+        
+        return scaledImage
+    }
 }
