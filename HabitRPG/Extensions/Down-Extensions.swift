2451eeb5f37adff89f79e9fdf00f5effbf36728c
Phillip Thelen <phillip@thelen.space>
2023-02-07 13:49:50 +0100

    Fixes #1268

--- HabitRPG/Extensions/Down-Extensions.swift
+++ HabitRPG/Extensions/Down-Extensions.swift
@@ -193,7 +193,7 @@ private class HabiticaStyler: DownStyler {
     
     override func style(codeBlock str: NSMutableAttributedString, fenceInfo: String?) {
         str.addAttributes([
-            .font: UIFontMetrics.default.scaledFont(for: UIFont(name: "Menlo", size: baseSize) ?? UIFont.systemFont(ofSize: baseSize))
+            .font: UIFontMetrics.default.scaledFont(for: UIFont(name: "Menlo", size: baseSize) ?? UIFont.systemFont(ofSize: baseSize)),            .foregroundColor: ThemeService.shared.theme.primaryTextColor
             ], range: NSRange(location: 0, length: str.length))
     }
 
