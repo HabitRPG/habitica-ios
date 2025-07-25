22be8e2461e9e4ea0c057bb51bf8f3952f48efad
Phillip Thelen <phillip@habitica.com>
2025-07-25 13:46:14 +0200

    get username from jwt

--- HabitRPG/UI/Onboarding/LoginViewModel.swift
+++ HabitRPG/UI/Onboarding/LoginViewModel.swift
@@ -12,6 +12,35 @@ import AppAuth
 import AuthenticationServices
 import Habitica_Models
 
+func decode(jwtToken jwt: String) -> [String: Any] {
+  let segments = jwt.components(separatedBy: ".")
+  return decodeJWTPart(segments[1]) ?? [:]
+}
+
+func base64UrlDecode(_ value: String) -> Data? {
+  var base64 = value
+    .replacingOccurrences(of: "-", with: "+")
+    .replacingOccurrences(of: "_", with: "/")
+
+  let length = Double(base64.lengthOfBytes(using: String.Encoding.utf8))
+  let requiredLength = 4 * ceil(length / 4.0)
+  let paddingLength = requiredLength - length
+  if paddingLength > 0 {
+    let padding = "".padding(toLength: Int(paddingLength), withPad: "=", startingAt: 0)
+    base64 = base64 + padding
+  }
+  return Data(base64Encoded: base64, options: .ignoreUnknownCharacters)
+}
+
+func decodeJWTPart(_ value: String) -> [String: Any]? {
+  guard let bodyData = base64UrlDecode(value),
+    let json = try? JSONSerialization.jsonObject(with: bodyData, options: []), let payload = json as? [String: Any] else {
+      return nil
+  }
+
+  return payload
+}
+
 class LoginViewModel: ObservableObject {
     weak var viewController: LoginTableViewController?
     private let userRepository = UserRepository()
@@ -58,6 +87,12 @@ class LoginViewModel: ObservableObject {
                 if authState != nil {
                     self?.socialLoginMethod = "google"
                     self?.socialLoginAccessToken = authState?.lastTokenResponse?.accessToken
+                    if let token = authState?.lastTokenResponse?.idToken {
+                        let content = decode(jwtToken: token)
+                        if let email = content["email"] as? String {
+                            self?.email = email
+                        }
+                    }
                     self?.userRepository.login(userID: "", network: "google", accessToken: self?.socialLoginAccessToken ?? "", allowRegister: false)
                         .observeValues { response in
                             if response?.newUser == true || response == nil {
@@ -102,6 +137,10 @@ class LoginViewModel: ObservableObject {
     }
     
     func performAppleLogin(identityToken: String, name: String) {
+        let content = decode(jwtToken: identityToken)
+        if let email = content["email"] as? String {
+            self.email = email
+        }
         socialLoginMethod = "apple"
         socialLoginAccessToken = identityToken
         userRepository.loginApple(identityToken: identityToken, name: name, allowRegister: false).observeResult {[weak self] (result) in
