bce9a0d599abcc12118b00a25a2404352c0417e7
Hafiz <hafizbhamidi@gmail.com>
2025-08-05 16:34:55 -0500

    Remove stable_Mount_Icon_Dragon-Hydra from formatDictionary

--- HabitRPG/Utilities/ImageManager.swift
+++ HabitRPG/Utilities/ImageManager.swift
@@ -60,8 +60,7 @@ class ImageManager {
         "shop_armoire": "gif",
         "Pet_HatchingPotion_Cryptid": "gif",
         "Mount_Head_Dragon-Hydra": "gif",
-        "Mount_Body_Dragon-Hydra": "gif",
-        "stable_Mount_Icon_Dragon-Hydra": "gif"
+        "Mount_Body_Dragon-Hydra": "gif"
     ]
     
     @MainActor
