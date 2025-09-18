65f3ce8918d447cf081c9909b8b03406d59b8221
Phillip Thelen <phillip@habitica.com>
2025-09-18 15:19:11 +0200

    fix merge

--- HabitRPG/Views/HabiticaBottomSheet.swift
+++ HabitRPG/Views/HabiticaBottomSheet.swift
@@ -15,8 +15,13 @@ protocol Dismissable {
 
 class HostingBottomSheetController<ContentView: View>: UIHostingController<ContentView> {
     private var bottomInset: CGFloat = 0
+    
+    private let allowLargeDetent: Bool
+    private let prefersGrabberVisible: Bool
         
     init(rootView: ContentView, allowLargeDetent: Bool = false, prefersGrabberVisible: Bool = true) {
+        self.allowLargeDetent = allowLargeDetent
+        self.prefersGrabberVisible = prefersGrabberVisible
         super.init(rootView: rootView)
         if let root = rootView as? Dismissable {
             root.dismisser.dismiss = {
@@ -25,10 +30,19 @@ class HostingBottomSheetController<ContentView: View>: UIHostingController<Conte
         }
         
         view.backgroundColor = .clear
+    }
+
+    @MainActor
+    required dynamic init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
         
         if let sheetController = self.presentationController as? UISheetPresentationController {
-            self.view.sizeToFit()
-            let actualViewSize = self.view.intrinsicContentSize.height
+            let size = self.view.sizeThatFits(CGSize(width: sheetController.containerView?.frame.size.width ?? 0, height: .greatestFiniteMagnitude))
+            let actualViewSize = size.height
             let fraction = UISheetPresentationController.Detent.custom { _ in
                 return actualViewSize + self.bottomInset
             }
@@ -38,10 +52,5 @@ class HostingBottomSheetController<ContentView: View>: UIHostingController<Conte
             }
             sheetController.prefersGrabberVisible = prefersGrabberVisible
         }
-    }
-
-    @MainActor
-    required dynamic init?(coder aDecoder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
     }
 }
