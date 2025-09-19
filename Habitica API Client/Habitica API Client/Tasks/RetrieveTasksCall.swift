35de33ae1c23cb776407ed593c77a7c308a02d01
Hafiz <hafizbhamidi@gmail.com>
2025-09-19 14:24:14 -0500

    Use en_US_POSIX format

--- Habitica API Client/Habitica API Client/Tasks/RetrieveTasksCall.swift
+++ Habitica API Client/Habitica API Client/Tasks/RetrieveTasksCall.swift
@@ -16,11 +16,13 @@ public class RetrieveTasksCall: ResponseArrayCall<TaskProtocol, APITask> {
         if let date = dueOnDay {
             let formatter = DateFormatter()
             formatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ssZZZZZ"
+            formatter.locale = Locale(identifier: "en_US_POSIX")
+            formatter.timeZone = TimeZone.current
             var dateString = formatter.string(from: date)
             let regex = try? NSRegularExpression(pattern: "T([0-9]):", options: .caseInsensitive)
             dateString = regex?.stringByReplacingMatches(in: dateString, options: [], range: NSRange(location: 0, length: dateString.count), withTemplate: "T0$1:") ?? ""
-            url = "\(url)?type=dailys&dueDate=\(dateString)"
-            url = url.replacingOccurrences(of: "+", with: "%2B")
+            let encodedDateString = dateString.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed) ?? dateString
+            url = "\(url)?type=dailys&dueDate=\(encodedDateString)"
         }
         if let type = type {
             url = "\(url)?type=\(type)"
