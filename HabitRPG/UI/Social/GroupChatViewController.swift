00a90357744964127b7a628bf76284651d250626
Fiz <hafizbhamidi@gmail.com>
2026-01-26 13:31:02 -0600

    Fix keyboard overlap with sent party chat message

--- HabitRPG/UI/Social/GroupChatViewController.swift
+++ HabitRPG/UI/Social/GroupChatViewController.swift
@@ -78,7 +78,9 @@ class GroupChatViewController: MessagesViewController {
             case .failure:
                 inputBar.inputTextView.text = text
             case .success:
-                return
+                if self.tableView.numberOfRows(inSection: 0) > 0 {
+                    self.tableView.scrollToRow(at: IndexPath(row: 0, section: 0), at: .top, animated: true)
+                }
             }
         }
     }
