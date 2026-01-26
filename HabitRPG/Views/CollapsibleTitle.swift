e2208d242c50b29c9a59ff1ba811e2f3948f8675
Fiz <hafizbhamidi@gmail.com>
2026-01-26 12:47:57 -0600

    faq bubble tweak

--- HabitRPG/Views/CollapsibleTitle.swift
+++ HabitRPG/Views/CollapsibleTitle.swift
@@ -198,6 +198,6 @@ class CollapsibleTitle: UIView, UIGestureRecognizerDelegate {
     }
 
     override var intrinsicContentSize: CGSize {
-        return CGSize(width: super.intrinsicContentSize.width, height: (subtitle != nil ? font.pointSize + subtitleFont.pointSize : font.pointSize) + 30)
+        return CGSize(width: super.intrinsicContentSize.width, height: (subtitle != nil ? font.pointSize + subtitleFont.pointSize : font.pointSize) + 38)
     }
 }
