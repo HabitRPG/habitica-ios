8d9726b2654970b66ec7fe8964c9051f205c12fa
Phillip Thelen <phillip@habitica.com>
2026-01-28 14:51:51 +0100

    don’t flash username screen on apple login

--- HabitRPG/Repositories/Implementations/UserRepository.swift
+++ HabitRPG/Repositories/Implementations/UserRepository.swift
@@ -250,7 +250,7 @@ class UserRepository: BaseRepository<UserLocalRepository> {
     func loginApple(identityToken: String, name: String, allowRegister: Bool) -> Signal<LoginResponseProtocol?, Never> {
         let call = AppleLoginCall(identityToken: identityToken, name: name, allowRegister: allowRegister)
         return call.objectSignal.merge(with: call.jsonSignal.map({ response -> LoginResponseProtocol? in
-            if let json = response as? [String: Any], json["id_token"] != nil {
+            if let json = response as? [String: Any], json["id_token"] == nil {
                 return nil
             }
             let loginResponse = APILoginResponse()
