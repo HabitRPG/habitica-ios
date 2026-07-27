b171fdd77a0f85a39a162478b99283e563b10726
Fiz <34069775+Hafizzle@users.noreply.github.com>
2026-07-27 14:12:19 -0500

    Restore the website prompt for creating challenges

--- HabitRPG/UI/Challenges/ChallengeTableViewController.swift
+++ HabitRPG/UI/Challenges/ChallengeTableViewController.swift
@@ -253,10 +253,15 @@ class ChallengeTableViewController: BaseTableViewController, UISearchBarDelegate
     }
     
     @IBAction func addChallengeAction(_ sender: Any) {
-        let viewController = CreateChallengeViewController()
-        viewController.modalPresentationStyle = .formSheet
-        viewController.isModalInPresentation = true
-        self.present(viewController, animated: true)
+        let alert = HabiticaAlertController(title: L10n.createChallenge, message: L10n.createChallengeDescription)
+        alert.addAction(title: L10n.openWebsite, style: .default, isMainAction: true) { _ in
+            guard let url = URL(string: "https://habitica.com/challenges/myChallenges") else {
+                return
+            }
+            UIApplication.shared.open(url)
+        }
+        alert.addCloseAction()
+        alert.show()
     }
     
     @IBAction func searchButtonTapped(_ sender: Any) {
